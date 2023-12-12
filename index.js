d88f64241f00ecc6745725b89dc29fdbccdeae39
Emil <emiliano.milewski@gmail.com>
2023-12-12 18:53:27 +0000

    part 2 complete

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function getFileData(fileLocation) {
     });
     return fileData;
 };
-const inputLocation = "./input.txt";
+const inputLocation = "./Day1/input.txt";
 const inputData = getFileData(inputLocation);
 
 //Part 1;
@@ -19,17 +19,53 @@ const inputData = getFileData(inputLocation);
     // Numbers spelled out in letters also now count 
         // example 1ba2fiveoad = 15 because of "five" being the last number
 
+function convertTextToNum(text) {
+    switch(text) {
+        case "zer":
+            return 0;
+        case "on":
+            return 1;
+        case "tw":
+            return 2;
+        case "thre":
+            return 3;
+        case "fou":
+            return 4;
+        case "fiv":
+            return 5;
+        case "si":
+            return 6;
+        case "seve":
+            return 7;
+        case "eigh":
+            return 8;
+        case "nin":
+            return 9;
+        default:
+            return text;
+    };
+};
+
 const inputArray = inputData.split("\n");
 let sum = 0;
 inputArray.forEach((item) => {
-    const letterNum = /one|two|three|four|five|six|seven|eight|nine|zero/g
-    const firstRegexp = new RegExp("\d" + "|" + letterNum); 
-    const lastRegexp = new RegExp("")
-    const firstDigit = item.match(firstRegexp);
-    const lastDigit = item.match(/\d(?=\D+$)|\d$/g);
-    // const firstDigit = item.match(/\d/); // part1 code
-    // const lastDigit = item.match(/\d(?=\D+$)|\d$/g); // part1 code
+    // const letterNum = "(one|two|three|four|five|six|seven|eight|nine|zero)";
+    const letterNum = "(on(?=e)|tw(?=o)|thre(?=e)|fou(?=r)|fiv(?=e)|si(?=x)|seve(?=n)|eigh(?=t)|nin(?=e)|zer(?=o))"
+    const regExp = new RegExp(`\\d|${letterNum}`, "g");
+    const digits = item.match(regExp);
+
+    let firstDigit = digits[0];
+    let lastDigit = digits[digits.length-1];
+
+    firstDigit = convertTextToNum(firstDigit);
+    lastDigit = convertTextToNum(lastDigit);
+
     const newDigit = String(firstDigit)+String(lastDigit);
     sum += parseInt(newDigit);
+    // const firstDigit = item.match(/\d/); // part1 code
+    // const lastDigit = item.match(/\d(?=\D+$)|\d$/g); // part1 code
+    // const newDigit = String(firstDigit)+String(lastDigit); //part1 code
+    // sum += parseInt(newDigit); part1 code
 });
 console.log(sum);
+
